982aef8a3bd8781ae454c128d5c8c9367b7b217b
ERMILA <120913658+Ermila049@users.noreply.github.com>
2023-11-30 18:20:04 +0800

    Menambahkan navbar dan footer

---
+++ admin/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer py-4 bg-primary mt-5">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-4 text-lg-start" style="color: aliceblue;">Copyright &copy; Your Website 2023</div>
+            <div class="col-lg-4 my-3 my-lg-0">
+
+            </div>
+            <div class="col-lg-4 text-lg-end">
+                <a class="link-dark text-decoration-none me-3" href="#!" style="color: aliceblue;">Privacy Policy</a>
+                <a class="link-dark text-decoration-none" href="#!" style="color: aliceblue;">Terms of Use</a>
+            </div>
+        </div>
+    </div>
+</footer>
