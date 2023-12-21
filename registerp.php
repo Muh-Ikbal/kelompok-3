86507dedccf8553f7c3270ba1e4b807b7c7aba80
Nouzen-s <ryuu.seryu@gmail.com>
2023-12-21 13:49:18 +0800

    menambahkan oop login

--- registerp.php
+++ registerp.php
@@ -1,42 +1,11 @@
 <?php
 include("koneksi.php");
-
-class User
-{
-    protected $password;
-    public $username;
-    public $fullname;
-    public function __construct($password, $username, $fullname)
-    {
-        $this->password = $password;
-        $this->username = $username;
-        $this->fullname = $fullname;
-    }
-}
-
-class UserManager extends User
-{
-    protected $conn;
-    public function __construct($conn, $password, $username, $fullname)
-    {
-        parent::__construct($password, $username, $fullname);
-        $this->conn = $conn;
-    }
-
-    public function createUser()
-    {
-        $hashedPassword = password_hash($this->password, PASSWORD_BCRYPT);
-        $stmt = $this->conn->prepare("INSERT INTO tb_user (username, password, fullname) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $this->username, $hashedPassword, $this->fullname);
-        return $stmt->execute();
-    }
-}
-
+include("oop-login.php");
 if (isset($_POST['signup']) && $_POST['signup'] == 'Register') {
     $fname = $_POST['fname'];
     $uname = $_POST['uname'];
     $password = $_POST['pass'];
-    
+
     $userManager = new UserManager($conn, $password, $uname, $fname);
     $result = $userManager->createUser();
 
